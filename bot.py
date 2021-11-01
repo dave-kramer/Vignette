212706a45a3056ec5b3718ab07e88ebf2463ed3f
dave-kramer <87333067+dave-kramer@users.noreply.github.com>
2021-11-01 10:46:40 +0100

    bot main

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Vignette is on')
+
+# PUT BOT TOKEN
+client.run('TOKEN')
